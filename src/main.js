23f91dd4729b78a384d961880d4eebb00ec4e7f5
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-02-18 18:32:44 +0300

    home comp

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import router from "./routes";
+
+import { MdCard, MdButton, MdDialog, MdContent } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+
+Vue.use(MdCard);
+Vue.use(MdButton);
+Vue.use(MdDialog);
+Vue.use(MdContent);
+
+import Button from "./components/UI/button.vue";
+Vue.component("app-button", Button);
+
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App)
 }).$mount("#app");
